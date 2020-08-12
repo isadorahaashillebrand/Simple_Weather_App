9f3f02e9c83e86c3a4d400c83b27749673d08b1c
isadorahaashillebrand <isadorahaas@gmail.com>
2020-08-12 03:21:56 +0100

    ajustes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Country from "./Components/Country/Country";
 const App = () => {
   let [tempSymbol, setTempSymbol] = useState("C");
   const [cities, setCities] = useState([]);
-  let key = "8dfc07466d233800b655f7c54878e145";
+  let id = "8dfc07466d233800b655f7c54878e145";
 
   useEffect(() => {
     (async function fetchData() {
       const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/group?id=5368361,524901&units=imperial&appid=${key}`
+        `http://api.openweathermap.org/data/2.5/group?id=5368361,524901&units=imperial&appid=${id}`
       );
       const data = await response.json();
       console.log(data);
